7529979c0c1fb01d3236e300ecc1e63ad07b2afa
Andrii Kislitsyn <andrii.kislitsyn@datarobot.com>
2026-06-15 23:12:31 +0300

    Add ExternalContributorsConfig type and schema

--- src/types.ts
+++ src/types.ts
@@ -12,6 +12,10 @@ export interface DependabotConfig {
   auto_label: boolean;
 }
 
+export interface ExternalContributorsConfig {
+  auto_label: boolean;
+}
+
 export interface NotificationIcons {
   header?: string;
   branch?: string;
@@ -33,6 +37,7 @@ export interface OrgConfig {
   default_slack_channel?: string;
   reminders?: RemindersConfig;
   dependabot?: DependabotConfig;
+  external_contributors?: ExternalContributorsConfig;
   reactions?: ReactionsConfig;
   teams: Record<string, TeamConfig>;
   users?: Record<string, string>;
